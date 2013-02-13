7d3a55e23d77dd76d741b0a3e28538055bc6b4bc
Luiz Armesto <luiz.armesto@gmail.com>
2013-02-13 17:30:38 -0200

    fixing bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 sudo apt-get install nodejs npm python-dev -y
 
 # install local python packages
-pip install -r settings/requirements.txt
+pip install -r requirements.txt
 
 sudo npm install -g coffee-script@1.2
 sudo npm install -g less@1.3.1
